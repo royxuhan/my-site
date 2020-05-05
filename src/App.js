a20647f693911a55d782503316b17cd6b0065c08
Roy <royxuhan@gmail.com>
2020-05-05 17:46:42 -0400

    Done

---
+++ src/App.js
@@ -0,0 +1,57 @@
+// Roy Xu 2020
+
+import React, { Component } from 'react'
+import ReactGA from 'react-ga'
+import $ from 'jquery'
+import './App.css'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import About from './Components/About'
+import Portfolio from './Components/Portfolio'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    }
+
+    ReactGA.initialize('UA-85418667-3')
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }
+
+  // fetch personal data and populate input hash 
+  getResumeData () {
+    $.ajax({
+      url: '/resumeData.json',
+      dataType: 'json',
+      cache: false,
+      success: function (data) {
+        this.setState({ resumeData: data })
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.log(err)
+      }
+    })
+  }
+
+  // fetch resume data right after the app is initialized 
+  componentDidMount () {
+    this.getResumeData()
+  }
+
+  // render everything
+  render () {
+    return (
+      <div className='App' >
+        <Header data={this.state.resumeData.main} />
+        <About data={this.state.resumeData.main} />
+        <Portfolio data={this.state.resumeData.portfolio} />
+        <Footer data={this.state.resumeData.main} />
+      </div>
+    )
+  }
+}
+
+export default App
